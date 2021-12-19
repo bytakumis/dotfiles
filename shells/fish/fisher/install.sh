1617e389a2688ecc425f33e300a082979a4342ff
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2021-12-19 23:55:38 +0900

    update: 移動に関する不要なfish moduleを削除した

--- shells/fish/fisher/install.sh
+++ shells/fish/fisher/install.sh
@@ -7,5 +7,4 @@ echo '##################################################'
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install rafaelrinaldi/pure
 fisher install jethrokuan/z
-fisher install b4b4r07/enhancd
 
